81a5fb71fe144ef13123587f8c15d2a4740d9bb0
mp11111 <40764845+mp11111@users.noreply.github.com>
2018-07-02 16:22:38 +0300

    Create StudentSignalsRepository.cs

---
+++ ucubot/DBCode/StudentSignalsRepository.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using ucubot.Model;
+using Dapper;
+
+namespace ucubot.DBCode
+
+{
+    [Route("api/[controller]")]
+
+    public class StudentSignalsRepository : IStudentSignalsRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MySqlConnection _msqlConnection;
+        private readonly string _connectionString;
+
+        public StudentSignalsRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("BotDatabase");
+            _msqlConnection = new MySqlConnection(_connectionString);
+        }
+
+        public IEnumerable<StudentSignals> ShowStudentSignalsN()
+        {
+             try
+             {
+                _msqlConnection.Open();
+                var comm = "SELECT first_name AS FirstName, last_name AS LastName, SignalType, Count FROM student_signals;";
+                 var lst = _msqlConnection.Query<StudentSignals>(comm).AsList();
+                _msqlConnection.Close();
+                return lst;
+             }
+             catch (Exception e)
+             {
+                 Console.WriteLine(e);
+                 _msqlConnection.Close();
+                 return null;
+             }
+        }
+    }
+}
