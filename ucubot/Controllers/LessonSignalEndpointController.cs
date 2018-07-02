18367eabb47c0045c99f25f7cc665f6d5014e05c
mp11111 <40764845+mp11111@users.noreply.github.com>
2018-07-02 16:15:06 +0300

    Update LessonSignalEndpointController.cs

--- ucubot/Controllers/LessonSignalEndpointController.cs
+++ ucubot/Controllers/LessonSignalEndpointController.cs
@@ -1,56 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Net;
 using ucubot.Model;
+using Dapper;
+using ucubot.DBCode;
 
 namespace ucubot.Controllers
 {
     [Route("api/[controller]")]
     public class LessonSignalEndpointController : Controller
     {
-        private readonly IConfiguration _configuration;
+        private readonly ILessonSignalRepository _lessonSignalRepository;
 
-        public LessonSignalEndpointController(IConfiguration configuration)
+        public LessonSignalEndpointController(ILessonSignalRepository lessonSignalRepository)
         {
-            _configuration = configuration;
+            _lessonSignalRepository = lessonSignalRepository;
         }
 
         [HttpGet]
         public IEnumerable<LessonSignalDto> ShowSignals()
         {
-            var connectionString = _configuration.GetConnectionString("BotDatabase");
-            // TODO: add query to get all signals
-            return new LessonSignalDto[0];
+            return _lessonSignalRepository.ShowSignalsN();
         }
-        
+
         [HttpGet("{id}")]
         public LessonSignalDto ShowSignal(long id)
         {
-            // TODO: add query to get a signal by the given id
-            return null;
+            return _lessonSignalRepository.ShowSignalN(id);
         }
-        
+
         [HttpPost]
         public async Task<IActionResult> CreateSignal(SlackMessage message)
         {
-            var userId = message.user_id;
-            var signalType = message.text.ConvertSlackMessageToSignalType();
+            if (_lessonSignalRepository.CreateSignalN(message))
+            {
+                return Accepted();
+            }
+            else{
+                return BadRequest();
+            }
 
-            // TODO: add insert command to store signal
-            
-            return Accepted();
         }
-        
+
         [HttpDelete("{id}")]
         public async Task<IActionResult> RemoveSignal(long id)
         {
-            //TODO: add delete command to remove signal
-            return Accepted();
+            if ( _lessonSignalRepository.RemoveSignalN(id))
+                {
+                    return Accepted();
+                }
+                else{
+                    return BadRequest();
+                }
         }
     }
 }
